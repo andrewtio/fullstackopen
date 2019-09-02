df4f61f2ba06760528d623b406a0e872a5627b57
Andrew Prasetyo <andrew@alamat.com>
2019-09-02 16:54:54 +0700

    Course Contents step8

--- part2/course-contents/src/components/Total.js
+++ part2/course-contents/src/components/Total.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const Total = (props) => {
+  const exercises =
+    props.parts.map(part => part.exercises)
+
+  console.log("exercisesArray", exercises)
+
+  const totalExercises =
+    exercises.reduce((s, p) => s + p)
+
+  console.log("parts", props.parts)
+  console.log("total", totalExercises)
   return (
     <>
       <p>
-        <b>Total of exercises {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises +
-          props.parts[3].exercises}
+        <b>Total of exercises {totalExercises}
         </b>
       </p>
     </>
