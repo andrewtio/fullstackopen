21644282afa8685b508bd46db7389043dbcfc3b6
Andrew Prasetyo <andrew@alamat.com>
2019-09-02 15:37:25 +0700

    course contents step6

---
+++ part2/course-contents/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Course from './components/Course'
+
+const App = ({ course }) => {
+  return (
+    <>
+      <Course course={course} />
+    </>
+  )
+}
+
+export default App
---
+++ part2/course-contents/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+
+
+ReactDOM.render(<App course={course} />, document.getElementById('root'))
