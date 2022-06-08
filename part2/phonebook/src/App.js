3643d95295f33564cf4988c0120c26512ec32c0b
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-06-08 20:29:24 +0700

    feat: save to backend

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import Name from './components/Name'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Name from "./components/Name";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -11,89 +11,79 @@ const App = () => {
     // { name: 'Ada Lovelace', number: '39-44-5323523' },
     // { name: 'Dan Abramov', number: '12-43-234345' },
     // { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
   //console.log("persons.name", persons[0])
 
   useEffect(() => {
-    console.log("Effect")
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        console.log("Promise Fullfiled")
-        console.log("Response.data", response.data)
-        setPersons(response.data)
-      })
-  })
-
-  const personsToShow = persons.filter(person => {
-    const filterUpperCase = filter.toUpperCase()
-    const personUpperCase = person.name.toUpperCase()
-    return personUpperCase.includes(filterUpperCase)
+    console.log("Effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("Promise Fullfiled");
+      console.log("Response.data", response.data);
+      setPersons(response.data);
+    });
   });
 
-  console.log("PersonsToShow", personsToShow)
-  console.log("Persons", persons)
-  console.log("Includes", persons.includes('Arto Hellas'))
+  const personsToShow = persons.filter((person) => {
+    const filterUpperCase = filter.toUpperCase();
+    const personUpperCase = person.name.toUpperCase();
+    return personUpperCase.includes(filterUpperCase);
+  });
+
+  console.log("PersonsToShow", personsToShow);
+  console.log("Persons", persons);
+  console.log("Includes", persons.includes("Arto Hellas"));
 
   const rows = () =>
-    personsToShow.map((name, i) =>
-      <Name
-        key={i}
-        name={name}
-      />
-    )
+    personsToShow.map((name, i) => <Name key={i} name={name} />);
 
-  console.log("rows", rows())
+  console.log("rows", rows());
 
-  const addName = event => {
-    event.preventDefault()
-    // const nameArray =
-    //   newName
+  const addName = (event) => {
+    event.preventDefault();
 
     // console.log("previous persons", persons);
     //setPersons(persons.push({ name: newName }));
-    if (persons.some(e => e.name === newName)) {
+    if (persons.some((e) => e.name === newName)) {
       alert(`${newName} is already added to phonebook.`);
     }
 
-    if (persons.some(e => e.number === newNumber)) {
-      alert(`${newNumber} is already added to phonebook`)
+    if (persons.some((e) => e.number === newNumber)) {
+      alert(`${newNumber} is already added to phonebook`);
     }
 
-    const phonebookObject = {
+    const personObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
-    // setPersons(persons => [...persons, { name: newName }]); 
-    // Ini dipakai jika ingin menambahkan nilai pada array object dengan single value
-    setPersons(persons.concat(phonebookObject))
-    setNewNumber('')
-    setNewName('');
-    console.log("phonebook Object", phonebookObject);
-  }
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then((response) => {
+        setPersons(persons.concat(response.data));
+        setNewNumber("");
+        setNewName("");
+      });
+  };
 
-  const handleNameChange = event => {
-    console.log("event.target.value Name", event.target.value)
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => {
+    console.log("event.target.value Name", event.target.value);
+    setNewName(event.target.value);
+  };
   // console.log("persons after add", persons)
 
-  const handleNumberChange = event => {
-    console.log("event.target.value Number", event.target.value)
-    setNewNumber(event.target.value)
-  }
+  const handleNumberChange = (event) => {
+    console.log("event.target.value Number", event.target.value);
+    setNewNumber(event.target.value);
+  };
 
   return (
     <>
       <h1>Phonebook</h1>
-      <Filter
-        filter={filter}
-        setFilter={setFilter}
-      />
+      <Filter filter={filter} setFilter={setFilter} />
       <h2>Add a New</h2>
       <PersonForm
         newName={newName}
@@ -106,7 +96,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons rows={rows} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
