4fac6b480056d439ccbbd8811eff6857806658db
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-07-06 17:59:03 +0700

    feat: Phonebook step12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,30 +5,19 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Notification from "./components/Notification";
+import ErrorMessage from "./components/ErrorMessage";
 import personService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas', number: '040-123456' },
-    // { name: 'Ada Lovelace', number: '39-44-5323523' },
-    // { name: 'Dan Abramov', number: '12-43-234345' },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
-  //console.log("persons.name", persons[0])
   const [notificationMessage, setNotificationMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     console.log("Effect");
-    // Before update using axios direct
-    // axios.get("http://localhost:3001/persons").then((response) => {
-    //   console.log("Promise Fullfiled");
-    //   console.log("Response.data", response.data);
-    //   setPersons(response.data);
-    // });
-
     personService.getAll().then((initialPersons) => {
       setPersons(initialPersons);
     });
@@ -40,23 +29,44 @@ const App = () => {
     return personUpperCase.includes(filterUpperCase);
   });
 
-  // console.log("PersonsToShow", personsToShow);
-  // console.log("Persons", persons);
-  // console.log("Includes", persons.includes("Arto Hellas"));
-
   const rows = () =>
-    personsToShow.map((name, i) => <Name key={i} name={name} />);
+    personsToShow.map((name, i) => (
+      <Name key={i} name={name} handleDeleteName={handleDeleteName} />
+    ));
 
-  // console.log("rows", rows());
+  const handleDeleteName = (id) => {
+    if (window.confirm("Do you really want to delete?")) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setNotificationMessage(
+            `Person '${
+              persons.find((person) => person.id === id).name
+            }' was already deleted`
+          );
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        })
+        .catch((error) => {
+          setErrorMessage(
+            `Information of '${
+              persons.find((person) => person.id === id).name
+            }' has already been removed from server`
+          );
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
+      setPersons(persons.filter((person) => person.id !== id));
+    }
+  };
 
-  const addName = (event) => {
+  const handleAddName = (event) => {
     event.preventDefault();
     const person = persons.find((n) => n.name === newName);
     const changedPerson = { ...person, number: newNumber };
 
-    // console.log("goobang", personId.id);
-    // console.log("changedPerson", changedPerson);
-    //setPersons(persons.push({ name: newName }));
     if (persons.some((e) => e.name === newName)) {
       if (
         window.confirm(
@@ -92,26 +102,12 @@ const App = () => {
         setNewNumber("");
       });
     }
-
-    // if (persons.some((e) => e.number === newNumber)) {
-    //   alert(`${newNumber} is already added to phonebook`);
-    // }
-
-    // Before update using direct axios
-    // axios
-    //   .post("http://localhost:3001/persons", personObject)
-    //   .then((response) => {
-    //     setPersons(persons.concat(response.data));
-    //     setNewNumber("");
-    //     setNewName("");
-    //   });
   };
 
   const handleNameChange = (event) => {
     console.log("event.target.value Name", event.target.value);
     setNewName(event.target.value);
   };
-  // console.log("persons after add", persons)
 
   const handleNumberChange = (event) => {
     console.log("event.target.value Number", event.target.value);
@@ -122,12 +118,13 @@ const App = () => {
     <>
       <h1>Phonebook</h1>
       <Notification message={notificationMessage} />
+      <ErrorMessage message={errorMessage} />
       <Filter filter={filter} setFilter={setFilter} />
       <h2>Add a New</h2>
       <PersonForm
         newName={newName}
         newNumber={newNumber}
-        addName={addName}
+        addName={handleAddName}
         handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange}
       />
--- part2/phonebook/src/components/Name.js
+++ part2/phonebook/src/components/Name.js
@@ -1,25 +1,15 @@
 import React from "react";
-import personService from "../services/persons";
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Name = ({ name }) => {
+const Name = ({ name, handleDeleteName }) => {
   // console.log("name.name", name.name.includes(persons))
   return (
     <div>
       {name.name} {name.number}{" "}
-      <Button
-        handleClick={() => {
-          if (window.confirm("Do you really want to delete?")) {
-            personService.deletePerson(name.id).then(() => {
-              alert(`Person '${name.name}' was already deleted`);
-            });
-          }
-        }}
-        text={"delete"}
-      />
+      <Button handleClick={() => handleDeleteName(name.id)} text={"delete"} />
     </div>
   );
 };
