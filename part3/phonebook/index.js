614e3231b5ea771ced1e1f709cfbea5796c59576
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-07-13 18:19:00 +0700

    feat: exercise 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
-app.use(express.json());
 
 let persons = [
   {
@@ -25,6 +24,10 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+app.use(express.json());
+
+app.use(morgan("combined"));
 
 const generateRandomId = () => {
   return Math.floor(Math.random() * 100000) + 1;
@@ -88,6 +91,12 @@ app.delete("/api/persons/:id", (request, response) => {
   console.log("persons", persons);
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
