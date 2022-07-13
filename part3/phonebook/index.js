69daeae2f094b79e6347c58360e98f7763c74151
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-07-13 18:30:26 +0700

    feat: exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,7 +27,15 @@ let persons = [
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("content", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
 
 const generateRandomId = () => {
   return Math.floor(Math.random() * 100000) + 1;
