5136fbda0f5d5518cdf4f7f176ca3dade4d3a74c
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-07-25 17:17:44 +0700

    3.11 phonebook full stack

---
+++ part3/phonebook/build/precache-manifest.5f9310448c417c3634cebd1391c462aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2e3326f5e7b8f11e9d8d9b916a224f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5378382b91e5d63aa0e",
+    "url": "/static/css/main.bd7d433b.chunk.css"
+  },
+  {
+    "revision": "b05abe2f9f5c63244205",
+    "url": "/static/js/2.3c9ed7f7.chunk.js"
+  },
+  {
+    "revision": "a5378382b91e5d63aa0e",
+    "url": "/static/js/main.572dff56.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
