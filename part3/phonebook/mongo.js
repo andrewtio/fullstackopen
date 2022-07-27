b21f0ffac542c0a1e8c2326c329d1c6324e967bd
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-07-27 18:25:19 +0700

    execise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+
+const url = `mongodb+srv://phonebook:${password}@cluster0.vtyqg.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+console.log(process.argv.length);
+
+if (process.argv.length === 5) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log("Connected Create");
+
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      });
+
+      return person.save();
+    })
+    .then((result) => {
+      console.log(`Added ${result.name} number ${result.number} to phonebook`);
+      return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+} else if (process.argv.length === 3) {
+  mongoose.connect(url).then((result) => {
+    console.log("Connected Find");
+
+    Person.find()
+      .then((result) => {
+        result.forEach((person) => {
+          console.log(person);
+        });
+        mongoose.connection.close();
+      })
+      .catch((err) => console.log(err));
+  });
+}
