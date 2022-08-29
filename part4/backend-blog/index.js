daf8dce207d99a2f1932671f290653f47859d92b
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-08-29 20:34:42 +0700

    Exercise 4.2

--- part4/backend-blog/index.js
+++ part4/backend-blog/index.js
@@ -1,47 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const Blog = require("./models/blog");
-const logger = require("./utils/logger");
+const app = require("./app");
+const http = require("http");
 const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB:", error.message);
-  });
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const body = request.body;
-  console.log("body", body);
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  });
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
