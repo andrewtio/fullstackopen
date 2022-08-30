f82b498b882045601bd1bf1685b8579884458d30
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-08-30 20:42:14 +0700

    Exercise 4.4: helper func and unit test step 2

--- part4/backend-blog/utils/list_helper.js
+++ part4/backend-blog/utils/list_helper.js
@@ -4,11 +4,28 @@ const dummy = (blogs) => {
     console.log("item", item);
     return sum + item;
   };
-  console.log("reducer", reducer);
 
   return blogs.length === 0 ? 1 : blogs.reduce(reducer, 0) / blogs.length;
 };
 
+const totalLikes = (likes) => {
+  console.log("likes", likes);
+  const result = likes.map((likes) => {
+    return likes.likes;
+  });
+  console.log("result", result);
+
+  const reducer = (sum, result) => {
+    console.log("sum", sum);
+    console.log("result", result);
+    return sum + result;
+  };
+
+  console.log("reducer", result.reduce(reducer, 0));
+  return likes.length === 0 ? 0 : result.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
