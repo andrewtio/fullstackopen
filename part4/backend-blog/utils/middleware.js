9e81cc4b2c08f689b05e05c8a87a28aee72e5e6a
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-11-16 19:58:34 +0700

    exercise 4.22

--- part4/backend-blog/utils/middleware.js
+++ part4/backend-blog/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method: ", request.method);
@@ -38,9 +40,26 @@ const tokenExtractor = (request, response, next) => {
   next();
 };
 
+const userExtractor = async (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    request.token = authorization.substring(7);
+  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  console.log("decodedToken", decodedToken);
+
+  const user = await User.findById(decodedToken.id);
+
+  request.user = user._id.toString();
+
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
+  userExtractor,
 };
