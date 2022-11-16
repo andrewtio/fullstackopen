5a854e2d22c8e77b8fea5cdfe3be338f45d08464
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-11-16 19:25:34 +0700

    exercise 4.21, delete console.log

--- part4/backend-blog/controllers/blogs.js
+++ part4/backend-blog/controllers/blogs.js
@@ -72,9 +72,6 @@ blogsRouter.delete("/:id", async (request, response) => {
   const user = await User.findById(decodedToken.id);
   const blog = await Blog.findById(id).populate("user");
 
-  console.log("user", user);
-  console.log("blog", blog);
-
   if (user._id.toString() !== blog.user._id.toString()) {
     return response.status(401).json({
       error: "You are not authorized to delete this blog",
