9b6a4feb0d79ea6794367b0b88d2d791378e98f6
andrewtioo <andrew.prasetyo@wahyoo.com>
2023-01-11 20:22:04 +0700

    fix: Exercise 5.8

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -50,7 +50,7 @@ const App = () => {
     console.log("id", id);
     const blog = blogs.find((n) => n.id === id);
     console.log("blog", blog);
-    const changedBlog = { blog, likes: blog.likes + 1 };
+    const changedBlog = { ...blog, likes: blog.likes + 1 };
     console.log("changedBlog", changedBlog);
 
     blogService.update(id, changedBlog).then((returnedBlog) => {
@@ -62,7 +62,7 @@ const App = () => {
       setTimeout(() => {
         setMessage(null);
       }, 5000);
-      window.location.reload();
+      // window.location.reload();
     });
   };
 
