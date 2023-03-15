9bfa1f3a036b2da9ecffd56518ecd5fea253b352
andrewtioo <andrew.prasetyo@wahyoo.com>
2023-03-15 20:04:02 +0700

    exercise 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -25,7 +25,9 @@ const App = () => {
       <button onClick={(e) => store.dispatch({ type: "GOOD" })}>good</button>
       <button onClick={(e) => store.dispatch({ type: "OK" })}>ok</button>
       <button onClick={(e) => store.dispatch({ type: "BAD" })}>bad</button>
-      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>zero</button>
+      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>
+        reset stats
+      </button>
       <div>good {store.getState().good}</div>
       <div>ok {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -6,6 +6,7 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action);
+  console.log("state", state);
   switch (action.type) {
     case "GOOD":
       return { ...state, good: state.good + 1 };
